1c2deb0e7b702b35ca52369fed26a6d6507f9578
Chris Loew <cloew123@gmail.com>
2014-08-25 12:04:41 -0600

    Added Endpoint to allow the retrieval of game data

--- src/Server/routes.py
+++ src/Server/routes.py
@@ -1,4 +1,5 @@
 from Server.Controller.start_game_controller import StartGameController
+from Server.Controller.get_game_controller import GetGameController
 from Server.Controller.guess_controller import GuessController
 from Server.Controller.start_next_round_controller import StartNextRoundController
 
@@ -7,5 +8,6 @@ from kao_flask.controllers.html_controller import HTMLController
 
 routes = [Endpoint('/', get=HTMLController('Server/templates/index.html')),
           Endpoint('/api/startgame', post=StartGameController()),
+          Endpoint('/api/<int:gameId>', get=GetGameController()),
           Endpoint('/api/<int:gameId>/guess', put=GuessController()),
           Endpoint('/api/<int:gameId>/nextround', put=StartNextRoundController())]
